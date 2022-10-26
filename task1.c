f7d582d3f8a3221812a20bb2b42dd466443cd82e
bczhc <bczhc0@126.com>
2022-10-26 15:31:59 +0800

    task1

---
+++ task1.c
@@ -0,0 +1,15 @@
+//
+// Created by bczhc on 25/10/22.
+//
+
+#include <stdio.h>
+
+int mian() {
+    int a, b;
+    printf("请输入矩形的长和宽："), fflush(stdout);
+    // TODO: invalid input and out of range handling
+    scanf("%d %d", &a, &b); // NOLINT(cert-err34-c)
+    printf("矩形的面积是：%d\n", a * b);
+
+    return 0;
+}
