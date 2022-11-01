dc15a3fbe214c95ee09f528781b2dc9bd3c95334
bczhc <bczhc0@126.com>
2022-11-01 08:01:37 +0800

    delete old code in demo.c

--- demo.c
+++ demo.c
@@ -14,24 +14,7 @@
 #include <stdbool.h>
 
 int main() {
-    String line = read_line(stdin);
 
-    printf("Input: %s\n", string_data(&line));
-
-    ParseIntResult parsed = parse_int(string_data(&line));
-
-    if (!parsed.ok) {
-        switch (parsed.data.error) {
-            case PIE_NO_DIGITS:
-                perror("PIE_NO_DIGITS");
-                break;
-            case PIE_OUT_OF_RANGE:
-                perror("PIE_OUT_OF_RANGE");
-                break;
-        }
-    } else {
-        printf("Parsed: %lu\n", parsed.data.result);
-    }
 
     return 0;
 }
